76346b0b6f95758b1a7007ed8c4dc2ba69fc5f0e
Robert Rogaliński <rrogalinski@linux-p110-15.inf.ug.edu.pl>
2017-11-19 13:58:59 +0100

    zad8

---
+++ zad8/zad8.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+double a= 1.0/0.0;
+double b= -1.0/0.0;
+double c= 0.0/0.0;
+printf("%lf\n", a);
+printf("%lf\n", b);
+printf("%lf\n", c);
+
+
+  return 0;
+}
